e8c1c47ce864c20694720f3c287050a5751b264c
oelbanna026 <89281320+oelbanna026@users.noreply.github.com>
2026-02-01 00:58:10 +0300

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'acrylic-app-v29';
+const CACHE_NAME = 'acrylic-app-v30';
 const ASSETS = [
     './',
     './index.html',
     './css/style.css',
-    './js/app.bundle.js?v=7',
+    './js/app.bundle.js?v=8',
     './js/auth.js?v=22',
     './js/tour.js?v=22',
     './js/monetization.js?v=22',
