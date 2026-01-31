13b723c6c10fbb90ffbdad711d1eff3e0abc028e
oelbanna026 <89281320+oelbanna026@users.noreply.github.com>
2026-01-31 14:00:47 +0300

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = process.env.DB_PATH || path.resolve(__dirname, '../config/database.sqlite');
+const dbPath = process.env.DB_PATH || path.resolve(__dirname, '../data/database.sqlite');
 
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
